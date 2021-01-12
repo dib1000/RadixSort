b102e672b625215dbcca5ca2b6ed37f8a0e944ab
Emma Buller <dib5017@gmail.com>
2021-01-12 18:46:32 -0500

    tested length method

---
+++ Radix.java
@@ -0,0 +1,12 @@
+public class Radix {
+  public static int nth(int n, int col) {
+    return Math.abs(n/((int)Math.pow(10,col)))%10;
+  }
+
+  public static int length(int n) {
+    if(n==0) {
+      return 1;
+    }
+    return (int)Math.log10(Math.abs(n))+1;
+  }
+}
